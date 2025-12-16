19a039a535320aece9c146a69936db628274d55c
nader4375 <archatayanader@gmail.com>
2025-12-16 19:44:23 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ fontFamily: "system-ui", padding: 24 }}>
+      <h1>Trebal Customer Portal</h1>
+      <p>Status: OK</p>
+    </div>
+  );
+}
